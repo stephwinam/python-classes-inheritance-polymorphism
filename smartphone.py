ad4af1aaa9a0de37ceae1304171783f0a27f332b
stephwinam <stephaniewinam@gmail.com>
2025-04-13 15:39:16 +0300

    Added attributes and methods

--- smartphone.py
+++ smartphone.py
@@ -7,4 +7,24 @@ class Smartphone:
         self.battery_capacity = battery_capacity
         self.is_on = False  # Initial state: smartphone is off 
 
+    def turn_on(self):
+        if not self.is_on:
+            self.is_on = True
+            print(f"{self.brand} {self.model} is now on.")
+        else:
+            print(f"{self.brand} {self.model} is already on.")
+
+    def turn_off(self):
+        if self.is_on:
+            self.is_on = False
+            print(f"{self.brand} {self.model} is now off.")
+        else:
+            print(f"{self.brand} {self.model} is already off.")
+
+    def take_photo(self):
+        if self.is_on:
+            print(f"Taking a photo with {self.camera_resolution} camera.")
+        else:
+            print("Please turn on the phone first.")
+
        
