5227e248772d0a4cf760ed53dac20d797b56848d
stephwinam <stephaniewinam@gmail.com>
2025-04-13 15:33:52 +0300

    Set up a smartphone class

---
+++ smartphone.py
@@ -0,0 +1,10 @@
+class Smartphone:
+    def __init__(self, brand, model, storage, camera_resolution, battery_capacity):
+        self.brand = brand
+        self.model = model
+        self.storage = storage
+        self.camera_resolution = camera_resolution
+        self.battery_capacity = battery_capacity
+        self.is_on = False  # Initial state: smartphone is off 
+
+       
